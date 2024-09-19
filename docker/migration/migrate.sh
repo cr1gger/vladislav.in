adc1f7803e7dd35443407ee6e563b9531667a66e
cr1gger <cr1gger@ya.ru>
2024-09-19 23:26:33 +0500

    docker-compose.yml

--- docker/migration/migrate.sh
+++ docker/migration/migrate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 whoami
+id
 wait-for-it ${MYSQL_HOST}:${MYSQL_PORT} && cd /app && php yii migrate --interactive=0 && php yii migrate-control --interactive=0 && php yii control/user/create-root && exit
