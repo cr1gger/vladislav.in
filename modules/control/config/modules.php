faafe0dbae9915d4a8c3120a492f32412694009c
cr1gger <cr1gger@ya.ru>
2024-10-17 21:56:06 +0500

    fix docker

--- modules/control/config/modules.php
+++ modules/control/config/modules.php
@@ -4,15 +4,20 @@ $modulesDir = __DIR__ . '/../modules';
 
 // Автозагрузка модулей без преднастроек
 $config = [];
-foreach(scandir($modulesDir) as $moduleDir) {
-    if ($moduleDir == '.' || $moduleDir == '..') {
-        continue;
-    }
-
-    $config[$moduleDir] = [
-        'class' => "app\modules\control\modules\\{$moduleDir}\Module"
-    ];
-}
+//foreach(scandir($modulesDir) as $moduleDir) {
+//    if ($moduleDir == '.' || $moduleDir == '..') {
+//        continue;
+//    }
+//
+//    $class = "app\modules\control\modules\\{$moduleDir}\Module";
+//
+//    if (class_exists($class)) {
+//        $config[$moduleDir] = [
+//            'class' => "app\modules\control\modules\\{$moduleDir}\Module"
+//        ];
+//    }
+//
+//}
 
 // Подключение без автозагрузки
 //return [
