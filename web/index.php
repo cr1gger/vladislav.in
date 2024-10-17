bb79f1fb0b8b45b0c09bdc9cc8f29b450c3655c0
cr1gger <cr1gger@ya.ru>
2024-10-17 20:26:19 +0500

    fix docker

--- web/index.php
+++ web/index.php
@@ -4,9 +4,6 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__.'/../.env');
-
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', (bool)$_ENV['YII_DEBUG']);
 defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV']);
