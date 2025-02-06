901b84eb6f9c48593418a5cdde98d250162d0c09
cr1gger <cr1gger@ya.ru>
2025-02-06 22:11:29 +0500

    TG BOT

--- modules/control/commands/GameController.php
+++ modules/control/commands/GameController.php
@@ -21,7 +21,7 @@ class GameController extends Controller
 
     public function actionStart()
     {
-        Console::output('Started!');
+        $this->sendTgMessage('Запущено!');
         $this->checkEnv();
 
         while (true) {
